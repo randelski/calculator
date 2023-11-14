755442f0f131b7a274d4ed229fcee95f5442cf2d
Randell <randz27tycoding@gmail.com>
2023-11-14 18:51:06 +0800

    Changed css of the display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        document.title = 'Calculator'; // Change the title when the component mounts
+    }, []);
+
+    const createDigits = () => {
+        const digits = [];
+
+        for (let i = 1; i < 10; i++) {
+            digits.push(
+                <button key={i}>{i}</button>
+            )
+        }
+        return digits;
+    }
+
+    return (
+        <div className="App">
+            <div className="calculator">
+                <div className="display">
+                    <span>(0)</span> 0
+                </div>
+
+                <div className="operators">
+                    <button style={{ backgroundColor: 'blue', color: 'white', padding: '10px 20px', border: 'none' }}>
+                        Click me
+                    </button>
+                    <button>*</button>
+                    <button>+</button>
+                    <button>-</button>
+
+                    <button>DEL</button>
+                </div>
+
+                <div className="digits">
+                    {createDigits()}
+                    <button>0</button>
+                    <button>.</button>
+                    <button>=</button>
+                </div>
+
+            </div>
+
+        </div >
+    );
 }
 
 export default App;
